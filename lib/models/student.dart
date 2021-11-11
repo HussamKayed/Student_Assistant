fe22f95cb0622d39860e6441fd8c675be1a7109b
Hussam Kayed <hossamkayed99@gmail.com>
2021-11-11 22:40:43 +0100

    Initial Setup

---
+++ lib/models/student.dart
@@ -0,0 +1,16 @@
+class Student {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final double hawGpa;
+  final double usGpa;
+  final double deGpa;
+
+  Student(
+      {required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.deGpa,
+      required this.hawGpa,
+      required this.usGpa});
+}
