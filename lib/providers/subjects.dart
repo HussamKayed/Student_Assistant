905e04bba3a068381196c67d5f3277c6cb3f36f8
Hussam Kayed <hossamkayed99@gmail.com>
2021-12-28 21:52:03 +0100

    V1 almost ready

--- lib/providers/subjects.dart
+++ lib/providers/subjects.dart
@@ -1,50 +1,128 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import '../data/endpoints.dart';
 import '../models/subject.dart';
+import 'package:http/http.dart' as http;
 
 class Subjects with ChangeNotifier {
-  List<Subject> subjects = [];
-  // Map<String, Subject> completedSubjects = {
-  //   "EE1" : Subject('EE1', ),
-  //   "MA1",
-  //   "SO1",
-  //   "GE",
-  //   "LSL1",
-  //   "LSE1",
-  //   "LSL2",
-  //   "MA2",
-  //   "SO2",
-  //   "EE2",
-  //   "EL1",
-  //   "IC",
-  //   "SS1",
-  //   "AD",
-  //   "EL2",
-  //   "DI",
-  //   "EM",
-  //   "SS2",
-  //   "SE",
-  //   "MC",
-  //   "DS",
-  //   "DB",
-  //   "SP",
-  //   "IP",
-  //   "BU",
-  //   "OS",
-  //   "DP",
-  //   "DC",
-  //   "CJ1",
-  //   "CM1",
-  //   "CM2",
-  //   "CJ2",
-  //   "BA",
-  // };
-  List<Subject> remainingSubjects = [];
+  Map<String, dynamic> completedSubjects = {};
+  Map<String, Subject> subjects = {
+    "EE1": Subject('EE1', 15),
+    "MA1": Subject('MA1', 15),
+    "SO1": Subject('SO1', 15),
+    "GE": Subject('GE', 0),
+    "LSL1": Subject('LSL1', 0),
+    "LSE1": Subject('LSE1', 0),
+    "LSL2": Subject('LSL2', 0),
+    "MA2": Subject('MA2', 15),
+    "SO2": Subject('SO2', 15),
+    "EE2": Subject('EE2', 15),
+    "EL1": Subject('EL1', 15),
+    "IC": Subject('IC', 0),
+    "SS1": Subject('SS1', 15),
+    "AD": Subject('AD', 15),
+    "EL2": Subject('EL2', 15),
+    "DI": Subject('DI', 15),
+    "EM": Subject('EM', 15),
+    "SS2": Subject('SS2', 15),
+    "SE": Subject('SE', 15),
+    "MC": Subject('MC', 15),
+    "DS": Subject('DS', 15),
+    "DB": Subject('DB', 15),
+    "SP": Subject('SP', 15),
+    "IP": Subject('IP', 0),
+    "BU": Subject('BU', 15),
+    "OS": Subject('OS', 15),
+    "DP": Subject('DP', 15),
+    "DC": Subject('DC', 15),
+    "CJ1": Subject('CJ1', 0),
+    "CM1": Subject('CM1', 15),
+    "CM2": Subject('CM2', 15),
+    "CJ2": Subject('CJ2', 15),
+    "BA": Subject('BA', 15),
+  };
+  Map<String, Subject> remainingSubjects = {};
 
-  List<Subject> get subjectsGetter {
-    return subjects;
+  int subjectsPerSemester = 0;
+
+  Map<int, int> completedSemesterSubjectsNumber = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+  };
+
+  // Map<String, Subject> get subjectsGetter {
+  //   return subjects;
+  // }
+
+  void getSemesterCompletedSubjects(Map<Subject, String> gradesMap) async {
+    final uri;
+    await filterSubject(gradesMap);
+    Map<String, String> headersMap = {};
+    gradesMap.forEach((subject, grade) {
+      final int key = subject.semester;
+      final int newValue = completedSemesterSubjectsNumber[key]! + 1;
+      completedSemesterSubjectsNumber[key] = newValue;
+      headersMap.addAll({subject.abbreviation: subject.hawGrade.toString()});
+    });
+
+    uri = Uri.https(Endpoints.mainEndpoint, '/transcript', headersMap);
+    completedSubjects = await http.get(uri, headers: {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    }).then((response) {
+      final responseBody = json.decode(response.body);
+      // print(responseBody);
+      return responseBody;
+    });
+    notifyListeners();
   }
 
-  void addSubject(Subject newSubject) {
-    subjects.add(newSubject);
+  Future filterSubject(Map<Subject, String> gradesMap) async {
+    var allSubjects = [];
+
+    allSubjects = await http
+        .get(Uri.parse(Endpoints.subjectsDetails))
+        .then((value) => json.decode(value.body)) as List;
+    gradesMap.forEach((subject, grade) {
+      for (var retrievedSubject in allSubjects) {
+        // print(retrievedSubject);
+        if (retrievedSubject['abbreviation'] == subject.abbreviation) {
+          subject.semester = retrievedSubject['semester'];
+          break;
+        }
+      }
+    });
+  }
+
+  Future getSubjectsNumberPerSemester(String semesterId) async {
+    final int subjectsPerSemester = await http
+        .get(Uri.parse(Endpoints.subjectsDetails + semesterId))
+        .then((response) {
+      return response.body.length;
+    });
+    return subjectsPerSemester;
+  }
+
+  void addSubject(Map<String, Subject> subject) {
+    completedSubjects.addAll(subject);
+    notifyListeners();
+  }
+
+  int get subjectsNumber {
+    return subjects.length;
+  }
+
+  int get completedSubjectsNumber {
+    int subjectsNumber = 0;
+    completedSemesterSubjectsNumber.forEach((key, value) {
+      subjectsNumber += value;
+    });
+    return subjectsNumber;
   }
 }
--- lib/screens/semesters_summary_screen.dart
+++ lib/screens/semesters_summary_screen.dart
@@ -5,9 +5,11 @@ import 'package:provider/provider.dart';
 
 import '../widgets/new_subject_widget.dart';
 import '/providers/subjects.dart';
+import '../models/subject.dart';
 
 class SemestersSummaryScreen extends StatefulWidget {
   static const String routeName = "/semesters-summary-page";
+  static int subjectsNumber = 0;
 
   @override
   State<SemestersSummaryScreen> createState() => _SemestersSummaryScreenState();
@@ -30,70 +32,94 @@ class _SemestersSummaryScreenState extends State<SemestersSummaryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Subjects(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('HAW CALCULATOR'),
-          centerTitle: true,
-          backgroundColor: Theme.of(context).primaryColor,
-        ),
-        body: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              SizedBox(height: 40),
-              Container(
-                  child: Center(
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          child: ProgressBarWidget())),
-                  height: MediaQuery.of(context).size.height * 0.2),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ProgressIndicatorWidget(20, 2),
-                  ProgressIndicatorWidget(20, 2),
-                  ProgressIndicatorWidget(20, 2),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ProgressIndicatorWidget(20, 2),
-                  ProgressIndicatorWidget(20, 2),
-                  ProgressIndicatorWidget(20, 2)
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [ProgressIndicatorWidget(20, 2)],
-              )
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(
-                  Icons.add,
-                  color: Colors.white,
+    final subject = Provider.of<Subjects>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HAW CALCULATOR'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Container(
+                child: Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: ProgressBarWidget(subject.completedSubjectsNumber /
+                        Subject.subjects.length),
+                  ),
                 ),
-                onPressed: () => startAdddingNewSubject(context),
-              ),
-              IconButton(
-                icon: const Icon(Icons.manage_accounts_sharp,
-                    color: Colors.white),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          color: Theme.of(context).primaryColor,
+                height: MediaQuery.of(context).size.height * 0.2),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[1]! /
+                        subject.subjectsPerSemester,
+                    1),
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[2]! /
+                        subject.subjects.length,
+                    2),
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[3]! /
+                        subject.subjects.length,
+                    3),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[4]! /
+                        subject.subjects.length,
+                    4),
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[5]! /
+                        subject.subjects.length,
+                    5),
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[6]! /
+                        subject.subjects.length,
+                    6)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ProgressIndicatorWidget(
+                    subject.completedSemesterSubjectsNumber[7]! /
+                        subject.subjects.length,
+                    7)
+              ],
+            )
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              onPressed: () => startAdddingNewSubject(context),
+            ),
+            IconButton(
+              icon:
+                  const Icon(Icons.manage_accounts_sharp, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        color: Theme.of(context).primaryColor,
       ),
     );
   }
